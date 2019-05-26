93ee0a77113aae55976133fdf364b3b248d7ff1a
CastaChick <casta46chick@gmail.com>
2019-05-26 23:05:24 +0900

    <ABC/128/D accept>

--- ABC/128/D.py
+++ ABC/128/D.py
@@ -4,8 +4,8 @@ V = list(map(int, input().split()))
 
 I = []
 
-for i in range(0, n+1):
-    for j in range(0, n+1-i):
+for i in range(min(n, k)+1):
+    for j in range(min(n, k)-i+1):
         I.append([i, j])
 
 Vr = V[::-1]
@@ -15,12 +15,11 @@ maxi = 0
 for l in I:
     H = V[0:l[0]] + Vr[0:l[1]]
     H.sort()
-    t = len(H)
-    re = k - t
-    c = 0
-    
-    sums = sum(H)
-    if maxi < sums:
-        maxi = sums
+    t = min(len(H), k - len(H))
+    t = max(0, t)
+    minus = sum(s < 0 for s in H)
+    score = sum(H[min(minus, t):])
+    maxi = max(maxi, score)
 
+print(maxi)
 
