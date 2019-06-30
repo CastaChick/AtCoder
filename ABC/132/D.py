9033ebdb8744fcd440671c4b13fd32c46463a032
CastaChick <casta46chick@gmail.com>
2019-06-30 15:19:38 +0900

    ABC/132 retry

--- ABC/132/D.py
+++ ABC/132/D.py
@@ -1,17 +1,28 @@
-import numpy as np
+global mod
+mod = 10 ** 9 + 7
+
+def mpow(x, n):  # x^nをmod取りながら返す
+    ans = 1
+    while n != 0:
+        if n & 1:
+            ans = ans * x % mod
+        x = x * x % mod
+        n = n >> 1
+
+    return ans
 
 n, k = map(int, input().split())
-mod = 10 ** 9 + 7
 
 ai = n - k + 1
 print(ai)
+
 for i in range(2, k +1):
     if n - k + 1 < i:
         print(0)
         continue
 
-    ai = ai * (n - k - i + 2) * (k - i + 1) / (i * (i - 1))
-    ai %= mod
-    ai = int(ai)
+    ai = ai * (n - k - i + 2) * (k - i + 1) % mod
+    ai = ai * mpow(i*(i-1), mod-2) % mod
 
     print(ai)
+
